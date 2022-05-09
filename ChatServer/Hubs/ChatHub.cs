0350a9337b738ff405b6749150a356724e2e9cfe
黄立铭 <frank@zasoft.cn>
2022-05-09 21:51:38 +0800

    finish first phrase
    frame work completed

--- ChatServer/Hubs/ChatHub.cs
+++ ChatServer/Hubs/ChatHub.cs
@@ -6,14 +6,43 @@ namespace ChatServer.Hubs
     public class ChatHub : Hub
     {
         private readonly string _bot;
-        public ChatHub()
+        private readonly IDictionary<string, UserConnection> _connections;
+        public ChatHub(IDictionary<string, UserConnection> connections)
         {
             _bot = "My Chat";
+            _connections = connections;
         }
         public async Task JoinRoom(UserConnection userConnection)
         {
+            _connections.TryAdd(Context.ConnectionId, userConnection);
             await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
             await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _bot, $"{userConnection.User} has joined {userConnection.Room}");
+            SendUsersConnected(userConnection.Room);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                _connections.Remove(Context.ConnectionId);
+                await Clients.Group(userConnection.Room).SendAsync($"{userConnection.User} has left");
+                SendUsersConnected(userConnection.Room); 
+            }
+        }
+
+        public async Task SendMessage(string message)
+        {
+            if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+            {
+                await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+            }
+        }
+
+        public Task SendUsersConnected(string room)
+        {
+            var users = _connections.Values.Where(c => c.Room == room).Select(c => c.User);
+            return Clients.Group(room).SendAsync("UsersInRoom", users);
+
         }
     }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,9 +1,26 @@
 using ChatServer.Hubs;
+using ChatServer.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(b =>
+    {
+        b.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials();
+    });
+});
+builder.Services.AddSingleton<IDictionary<string, UserConnection>>(opt => new Dictionary<string, UserConnection>());
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-app.MapHub<ChatHub>("/chat");
+app.UseCors();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chat");
+});
+
 app.Run();

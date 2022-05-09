b3549c5fa17b55b59cb45c7560d89b1475eabace
黄立铭 <frank@zasoft.cn>
2022-05-09 21:20:24 +0800

    Init project
    add hub

---
+++ ChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using ChatServer.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _bot;
+        public ChatHub()
+        {
+            _bot = "My Chat";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _bot, $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,6 +1,9 @@
+using ChatServer.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapHub<ChatHub>("/chat");
 app.Run();
